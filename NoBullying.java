524bd13483f9212c64778fa609e68601ca2c6c99
VITOR RUBIM PASSOS <52292234X@alunos.etec.itaqua>
2019-03-14 20:30:41 -0300

    adicionado todos 10 passos

--- NoBullying.java
+++ NoBullying.java
@@ -1,15 +1,15 @@
 public class NoBullying {
 	public static void main(String[] args){ 
-       System.out.println('Programa NoBullying');
-	   System.out.println('1ºPasso: Direcionar mais verba para auxílio psicológico nas escolas; ');
-	   System.out.println('2ºPasso: Ter mais atenção em pessoas que estão tendenciosas a sofrer bullying');
-	   System.out.println('3ºPasso: Não praticar');
-	   System.out.println('4ºPasso: Conversar e repreender quem pratica');
-	   System.out.println('5ºPasso: Não achar que Bullying é uma "brincadeira inofensiva"');
-	   System.out.println('6ºPasso: Ajudar o amigo que está agindo de forma estranha ');
-	   System.out.println('7ºPasso: Não incetivar quem pratica');
-	   System.out.println('8ºPasso: Aceitar diversidades da sociedade');
-	   System.out.println('9ºPasso: Denunciar quem pratica');
-	   System.out.println('10ºPasso: Pena severa ao crime. ');
+       System.out.println("Programa NoBullying");
+	   System.out.println("1ºPasso: Direcionar mais verba para auxílio psicológico nas escolas; ");
+	   System.out.println("2ºPasso: Ter mais atenção em pessoas que estão tendenciosas a sofrer bullying");
+	   System.out.println("3ºPasso: Não praticar");
+	   System.out.println("4ºPasso: Conversar e repreender quem pratica");
+	   System.out.println("5ºPasso: Não achar que Bullying é uma brincadeira inofensiva");
+	   System.out.println("6ºPasso: Ajudar o amigo que está agindo de forma estranha ");
+	   System.out.println("7ºPasso: Não incetivar quem pratica");
+	   System.out.println("8ºPasso: Aceitar diversidades da sociedade");
+	   System.out.println("9ºPasso: Denunciar quem pratica");
+	   System.out.println("10ºPasso: Pena severa ao crime. ");
 	}
 } 
